ef781f1ac61ff64c3b14afdf9bcedfdbeb277481
diahayuistiqomah <126170779+diahayuistiqomah@users.noreply.github.com>
2023-03-21 15:49:30 +0700

    update todo menggunakan api

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,73 +2,107 @@ const todoInput = document.getElementById("todoInput");
 const todoList = document.getElementById("todoList");
 const clearCompletedButton = document.getElementById("clearCompletedButton");
 
-const todos = JSON.parse(localStorage.getItem("todos")) || [];
+const urls = "https://crudcrud.com/api/42f64ec3e6ab4ada8540b22e9601bb1b";
 
 const renderTodos = () => {
   todoList.innerHTML = "";
-
-  todos.forEach((todo, index) => {
-    const todoItem = document.createElement("li");
-    todoItem.innerHTML = `
-      <div class="flex items-center">
-        <input type="checkbox" id="todo${index}" ${todo.completed ? "checked" : ""} class="mr-2" />
-        <label for="todo${index}" class="${todo.completed ? "line-through" : ""}">${todo.title}</label>
-        <button class="ml-auto px-2 py-1 rounded-md bg-red-400 text-white font-bold hover:bg-red-500" data-index="${index}">Delete</button>
-      </div>
-    `;
-    todoList.appendChild(todoItem);
-  });
-
-  localStorage.setItem("todos", JSON.stringify(todos));
+  fetch(`${urls}/todos`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      data.map((todo, index) => {
+        const todoItem = document.createElement("li");
+        todoItem.innerHTML = `
+          <div class="flex items-center">
+            <input type="checkbox" id="todo${index}" ${todo.completed ? "checked" : ""} class="mr-2" />
+            <label for="todo${index}" class="${todo.completed ? "line-through" : ""}">${todo.title}</label>
+            <button class="ml-auto px-2 py-1 rounded-md bg-red-400 text-white font-bold hover:bg-red-500" data-index="${index}">Delete</button>
+          </div>
+        `;
+        todoList.appendChild(todoItem);
+        const deleteButton = todoItem.querySelector("button");
+        //delete
+        deleteButton.addEventListener("click", () => {
+          fetch(`${urls}/todos/${todo._id}`, { method: "DELETE" })
+            .then(() => {
+              renderTodos();
+              console.log("succes delete");
+            })
+            .catch((error) => console.log(error));
+        });
+        //update
+        const checkbox = todoItem.querySelector("input");
+        checkbox.addEventListener("change", () => {
+          const newData = { title: todo.title, completed: !todo.completed };
+          fetch(`${urls}/todos/${todo._id}`, {
+            method: "PUT",
+            body: JSON.stringify(newData),
+            headers: { "Content-Type": "application/json" },
+          })
+            .then(() => {
+              renderTodos();
+              console.log("succes update");
+            })
+            .catch((error) => console.log(error));
+        });
+      });
+    })
+    .catch((error) => console.log(error));
 };
 
 const addTodo = () => {
   const todoTitle = todoInput.value.trim();
+
   if (todoTitle) {
-    todos.push({ title: todoTitle, completed: false });
-    renderTodos();
     todoInput.value = "";
+
+    const newData = { title: todoTitle, completed: false };
+    fetch(`${urls}/todos`, {
+      method: "POST",
+      body: JSON.stringify(newData),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        renderTodos();
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
   }
 };
-
-const deleteTodo = (index) => {
-  todos.splice(index, 1);
-  renderTodos();
-};
-
-const toggleTodoCompleted = (index) => {
-  todos[index].completed = !todos[index].completed;
-  renderTodos();
-};
-
-const clearCompletedTodos = () => {
-  for (let i = todos.length - 1; i >= 0; i--) {
-    if (todos[i].completed) {
-      todos.splice(i, 1);
-    }
-  }
-  renderTodos();
+//clear all completed
+const cleartodo = () => {
+  fetch(`${urls}/todos`)
+    .then((response) => response.json())
+    .then((data) => {
+      const newdata = data.filter((todo) => todo.completed);
+      const deletedata = newdata.map((todo) => {
+        fetch(`${urls}/todos/${todo._id}`, { method: "DELETE" })
+          .then(() => {
+            {
+              console.log("succes delete");
+            }
+          })
+          .catch((error) => console.log(error));
+      });
+      Promise.all(deletedata)
+        .then(() => {
+          console.log("succes delete all");
+          renderTodos();
+        })
+        .catch((error) => console.log(error));
+    })
+    .catch((error) => console.log(error));
 };
 
 document.addEventListener("submit", (event) => {
   event.preventDefault();
   addTodo();
 });
-
-todoList.addEventListener("click", (event) => {
-  if (event.target.tagName === "BUTTON") {
-    const index = event.target.dataset.index;
-    deleteTodo(index);
-  }
+//clear button
+clearCompletedButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  cleartodo();
 });
-
-todoList.addEventListener("change", (event) => {
-  if (event.target.tagName === "INPUT") {
-    const index = event.target.id.replace("todo", "");
-    toggleTodoCompleted(index);
-  }
-});
-
-clearCompletedButton.addEventListener("click", clearCompletedTodos);
 
 renderTodos();
